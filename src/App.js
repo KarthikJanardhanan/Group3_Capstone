7658c2b35f67cba96bc916a35ea2c45c2efa843a
sumitha thankachan <sumitha.thankachan@gmail.com>
2025-02-07 15:57:12 -0500

    login signup navbar removal

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Home from "./Components/Home";
 function App() {
   return (
     <div className="App">
-      <Header />
+   
       <Routes>
         <Route path="/" element={<LoginSignup />} />
         <Route path="/home" element={<Home />} />
